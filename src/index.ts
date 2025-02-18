464be10d88509e615b7a1da5492389c931bbef00
Mihkel Eidast <mihkel.eidast@telia.ee>
2025-02-18 11:23:33 +0200

    feat: add useScrollbarSize named export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-default-export */
-import useScrollbarSize from './useScrollbarSize';
+import { useScrollbarSize } from './useScrollbarSize';
 
 export type { ScrollbarMeasurements } from './useScrollbarSize';
+
+export { useScrollbarSize };
 
 export default useScrollbarSize;
